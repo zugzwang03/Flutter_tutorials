4633c36d532aaf586d6fda7d45be3920f8d25c9f
Shreya Sarkar <95460021+zugzwang03@users.noreply.github.com>
2023-06-21 09:38:11 +0530

    Add files via upload

---
+++ quote.dart
@@ -0,0 +1,5 @@
+class Quote {
+  String text = "S";
+  String author = "S";
+  Quote(this.text, this.author);
+}
---
+++ quote_card.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+  final VoidCallback delete;
+  QuoteCard({required this.quote, required this.delete});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+            ),
+            SizedBox(height: 8.0),
+            TextButton.icon(
+              onPressed: delete,
+              label: Text(
+                'Delete the icon',
+                style: TextStyle(color: Colors.black),
+              ),
+              icon: Icon(
+                Icons.delete,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
